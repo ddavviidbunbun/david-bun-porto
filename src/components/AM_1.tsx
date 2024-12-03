1e05feae51a706dc9a46a657886136216cfafeb4
DavidB <david.bunyamin.99@gmail.com>
2024-12-03 21:17:43 +0700

    changes-2

--- src/components/AM_1.tsx
+++ src/components/AM_1.tsx
@@ -1,4 +1,12 @@
 import "../assets/style.css";
+import cppimg from "/CPP-TS.png";
+import nodeimg from "/NodeJS-TS.png";
+import tsimg from "/TypeScript-TS.png";
+import reactimg from "/React-TS.png";
+import sqlimg from "/MySQL-TS.png";
+import pyimg from "/Python-TS.png";
+import gitimg from "/GitHub-TS.png";
+import linimg from "/LinkedIn-TS.png";
 
 export default function AM_1() {
   return (
@@ -9,27 +17,23 @@ export default function AM_1() {
             <span>Tech Stack</span>
           </div>
           <div className="atasTSIMG grid">
-            <img src="/CPP-TS.png" alt="CPP" className="gambarTS" />
-            <img src="/NodeJS-TS.png" alt="NodeJS" className="gambarTS" />
-            <img
-              src="/TypeScript-TS.png"
-              alt="TypeScript"
-              className="gambarTS"
-            />
-            <img src="/React-TS.png" alt="React" className="gambarTS" />
-            <img src="/MySQL-TS.png" alt="MySQL" className="gambarTS" />
-            <img src="/Python-TS.png" alt="Python" className="gambarTS" />
+            <img src={cppimg} alt="CPP" className="gambarTS" />
+            <img src={nodeimg} alt="NodeJS" className="gambarTS" />
+            <img src={tsimg} alt="TypeScript" className="gambarTS" />
+            <img src={reactimg} alt="React" className="gambarTS" />
+            <img src={sqlimg} alt="MySQL" className="gambarTS" />
+            <img src={pyimg} alt="Python" className="gambarTS" />
           </div>
         </div>
         <div className="flex justify-between">
           <a href="https://github.com/ddavviidbunbun">
             <div className="bg-[#292A2D] onlyRounded gambarGitIn flex justify-center items-center">
-              <img src="/GitHub-TS.png" alt="GitHub" className="imgGit" />
+              <img src={gitimg} alt="GitHub" className="imgGit" />
             </div>
           </a>
           <a href="https://www.linkedin.com/in/david-bunyamin-2303a033a/">
             <div className="bg-[#006699] onlyRounded gambarGitIn flex justify-center items-center">
-              <img src="/LinkedIn-TS.png" alt="LinkedIn" className="imgLin" />
+              <img src={linimg} alt="LinkedIn" className="imgLin" />
             </div>
           </a>
         </div>
--- src/components/AM_3.tsx
+++ src/components/AM_3.tsx
@@ -1,11 +1,12 @@
 import "../assets/style.css";
+import ppimg from "/Foto.png";
 
 export default function AM_3() {
   return (
     <>
       <div className="aboutMe-3 grid bg-[#292A2D] onlyRounded">
         <div>
-          <img src="/Foto.png" alt="Foto" className="onlyRounded-t" />
+          <img src={ppimg} alt="Foto" className="onlyRounded-t" />
         </div>
         <div className="flex justify-center items-start">
           <div className="font-hevBold">
